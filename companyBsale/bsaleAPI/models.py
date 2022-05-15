882d71f0a53573f89e5c304fcd47567f6e07c9dc
Anticucho <jorge_2jul@hotmail.com>
2022-05-15 03:20:26 -0400

    Creada conexion con db y funciones get para obtener data en json

--- companyBsale/bsaleAPI/models.py
+++ companyBsale/bsaleAPI/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    id =models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'category'
+
+
+class Product(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    url_image = models.CharField(max_length=255, blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
+    discount = models.IntegerField(blank=True, null=True)
+    category = models.ForeignKey(Category, models.DO_NOTHING, db_column='category', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'product'    
--- companyBsale/bsaleAPI/views.py
+++ companyBsale/bsaleAPI/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from .models import Category, Product
 
 # Create your views here.
+class ProductView(View):
+    def get(self, request):
+        products = list(Product.objects.values())
+        {'products': products}
+        return JsonResponse(products, safe=False)
+
+class CategoryView(View):
+    def get(self, request):
+        categories = list(Category.objects.values())
+        {'categories': categories}
+        return JsonResponse(categories, safe=False)
+
